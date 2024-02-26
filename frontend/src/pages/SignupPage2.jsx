d6ab101b8a23c3cf96b9190f2c730b1dba13a521
Avishake00 <sbardhan683@gmail.com>
2024-02-27 01:42:47 +0530

    auth

---
+++ frontend/src/pages/SignupPage2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignupForm2 from '../components/Auth/SingupForm2'
+
+const SignupPage2 = () => {
+  return (
+    <div>
+        <SignupForm2/>
+    </div>
+  )
+}
+
+export default SignupPage2
